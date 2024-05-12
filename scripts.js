0ed4256a33441b66930c1776c611d0124a0425dd
Grzegorz Maj <grzesiekm1992@gmail.com>
2024-05-12 22:23:40 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const totalEpisodes = 1103;
 
     // Dane o oglądaniu
-    let currentEpisode = 644;
+    let currentEpisode = 648;
     let startDate = new Date('2024-01-25');
 
     // Wybór elementów HTML
