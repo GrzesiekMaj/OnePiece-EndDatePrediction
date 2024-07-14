1847779bdb9fbf055d4cfca9caec5e449288a3e4
Grzegorz Maj <grzesiekm1992@gmail.com>
2024-07-15 00:15:10 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Dane odcinków One Piece
-    const totalEpisodes = 1109;
+    const totalEpisodes = 1110;
 
     // Dane o oglądaniu
-    let currentEpisode = 951;
+    let currentEpisode = 955;
     let startDate = new Date('2024-01-25');
 
     // Wybór elementów HTML
